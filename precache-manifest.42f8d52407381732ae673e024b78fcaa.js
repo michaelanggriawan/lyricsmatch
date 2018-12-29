babbe1b666f1ae0cf55066c86c4fc3582f0926cf
Michael Anggriawan <michaelanggriawan941@gmail.com>
2018-12-29 18:01:11 +0700

    Updates

---
+++ precache-manifest.42f8d52407381732ae673e024b78fcaa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2065359edfcf190225e6",
+    "url": "/lyricsmatch/static/js/runtime~main.2065359e.js"
+  },
+  {
+    "revision": "c5574856377d69387d05",
+    "url": "/lyricsmatch/static/js/main.c5574856.chunk.js"
+  },
+  {
+    "revision": "4a624083faa7d1e1569d",
+    "url": "/lyricsmatch/static/js/1.4a624083.chunk.js"
+  },
+  {
+    "revision": "c3a8fa16c031023c9e87c28b6dfcbcda",
+    "url": "/lyricsmatch/index.html"
+  }
+];
